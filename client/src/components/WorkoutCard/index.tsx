de0aacf88fa682d87b26ae60f11e6ab383d6ad79
Marcel Noack <marcel.noack.dev@gmail.com>
2021-01-24 16:26:57 +0100

    (feat): add basic model types and workout card

--- client/src/components/WorkoutCard/index.tsx
+++ client/src/components/WorkoutCard/index.tsx
@@ -1,18 +1,52 @@
 import React, { FunctionComponent } from 'react';
+import { Workout } from '../../context/types';
 import './index.css';
 
-export interface WorkoutCardProps {}
+export interface WorkoutCardProps {
+  workout: Workout;
+}
 
-const WorkoutCard: FunctionComponent<WorkoutCardProps> = () => {
+const WorkoutCard: FunctionComponent<WorkoutCardProps> = ({ workout }) => {
+  const { name, active, trainedAt, trainingInterval } = workout;
+
+  const getLatestDate = (dates: Array<Date>): Date => dates.sort((a, b) => b.getTime() - a.getTime())[0];
+
+  const getNextDate = (date: Date, interval: number): Date => new Date(date.setDate(date.getDate() + interval));
+
   return (
-    <article className='workout'>
-      <div className='workout__content'>
-        <h3>Full-Body B</h3>
-        <p>Test</p>
-      </div>
-      <button className='workout__nav ripple' onClick={() => console.log('TEST')}>
-        <i className='material-icons'>navigate_next</i>
+    <article className={`workout ${!active && 'error'}`}>
+      <article className='workout__wo-content'>
+        <div className='wo-content__header'>
+          <h3 className='pr-5'>{name}</h3>
+          <i className={`material-icons md-18 ${active ? 'success' : 'error'}`}>{active ? 'play_arrow' : 'pause'}</i>
+        </div>
+        <dl className='wo-content__wo-pairlist'>
+          <div className={`wo-pairlist__pair ${!active && 'error'}`}>
+            <dt>Last Training</dt>
+            <dd>{getLatestDate(trainedAt).toLocaleDateString()}</dd>
+          </div>
+          {active && (
+            <div className='wo-pairlist__pair'>
+              <dt>Next Training</dt>
+              <dd>{getNextDate(getLatestDate(trainedAt), trainingInterval).toLocaleDateString()}</dd>
+            </div>
+          )}
+          <div className={`wo-pairlist__pair ${!active && 'error'}`}>
+            <dt>Weight Increase since start</dt>
+            {/* TODO: calculate dynamically */}
+            <dd>+10%</dd>
+          </div>
+        </dl>
+      </article>
+      <button className='workout__wo-nav ripple' onClick={() => console.log('TEST')}>
+        <i className={`material-icons ${!active && 'error'}`}>navigate_next</i>
       </button>
+      {!active && (
+        <div className='overlay overlay--disabled'>
+          <span>Inactive</span>
+          <button onClick={() => console.log('Reactivate')}>Reactivate</button>
+        </div>
+      )}
     </article>
   );
 };
--- client/src/domain/workouts/index.tsx
+++ client/src/domain/workouts/index.tsx
@@ -1,8 +1,20 @@
 import React, { useContext, useEffect } from 'react';
 import WorkoutCard from '../../components/WorkoutCard';
 import { AppContext } from '../../context/AppContext';
+import { Workout } from '../../context/types';
 
 import './index.css';
+
+const WORKOUT: Workout = {
+  workoutId: 'randomString',
+  name: 'Full-Body B',
+  active: true,
+  trainedAt: [new Date(), new Date()],
+  trainingInterval: 2,
+  createdBy: 'MLNK',
+  createdAt: new Date(),
+  changedAt: new Date()
+};
 
 const Workouts = () => {
   const { dispatch } = useContext(AppContext);
@@ -13,14 +25,10 @@ const Workouts = () => {
 
   return (
     <>
-      <WorkoutCard />
-      <WorkoutCard />
-      <WorkoutCard />
-      <WorkoutCard />
-      <WorkoutCard />
-      <WorkoutCard />
-      <WorkoutCard />
-      <WorkoutCard />
+      <WorkoutCard key={WORKOUT.workoutId} workout={WORKOUT} />
+      <WorkoutCard key={WORKOUT.workoutId + 1} workout={{ ...WORKOUT, name: 'Full-Body A', active: false }} />
+      <WorkoutCard key={WORKOUT.workoutId + 1} workout={{ ...WORKOUT, name: 'Chest-Day A', active: false }} />
+      <WorkoutCard key={WORKOUT.workoutId} workout={{ ...WORKOUT, name: 'Legs A' }} />
     </>
   );
 };
