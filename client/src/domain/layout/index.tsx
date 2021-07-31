8975c37d44a65f7a121a3209e0c6fd9dd0ae6ddb
Marcel Noack <marcel.noack.dev@gmail.com>
2021-07-31 12:31:46 +0200

    (fix): ES-lint unused var

--- client/src/domain/layout/index.tsx
+++ client/src/domain/layout/index.tsx
@@ -4,26 +4,9 @@ import AppContextProvider from '../../context/AppContext';
 import AppBar from './AppBar';
 
 import './layout.css';
-import WorkoutContextProvider, { WorkoutContext } from '../../context/WorkoutContext';
-import { useLocation } from 'react-router-dom';
+import WorkoutContextProvider from '../../context/WorkoutContext';
 
 const Layout: FunctionComponent = ({ children }) => {
-  const [hasMainScrollbar, setHasMainScrollbar] = useState<boolean>(false);
-  const { workoutState } = useContext(WorkoutContext);
-  const location = useLocation();
-
-  useEffect(() => {
-    const checkScrollbar = () => {
-      setHasMainScrollbar(false);
-      const mainElement = document.getElementById('main');
-      if (mainElement && mainElement.scrollHeight > mainElement.clientHeight) {
-        setHasMainScrollbar(true);
-      }
-    };
-    checkScrollbar();
-    console.log('checked');
-  }, [location, workoutState]);
-
   return (
     <div className='app-layout'>
       <AppContextProvider>
