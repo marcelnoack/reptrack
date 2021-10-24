4ff5f123c4cf06d2e3d5ad2957068f15c11097fd
Marcel Noack <marcel.noack.dev@gmail.com>
2021-10-24 18:24:51 +0200

    (refactor): remove old server setup

--- server (old)/src/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import workout from './routes/workout';
-
-export default () => {
-  const app = Router();
-  workout(app);
-
-  return app;
-};
--- server/src/loaders/express.ts
+++ server/src/loaders/express.ts
@@ -9,4 +9,21 @@ export default ({ app }: { app: express.Application }) => {
   app.use(cors());
   app.use(bodyParser.json());
   app.use(`/${config.api.prefix}`, routes());
+
+  // catch 404 and forward to error handler
+  // app.use((req, res, next) => {
+  //   const err: Error = new Error('Not Found');
+  //   err['status'] = 404;
+  //   next(err);
+  // });
+
+  // // error handlers
+  // app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  //   res.status(err.status || 500);
+  //   res.json({
+  //     errors: {
+  //       message: err.message
+  //     }
+  //   });
+  // });
 };
