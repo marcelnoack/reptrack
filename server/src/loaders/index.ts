da362461f015e39487e1949761aeefc75cab8f9b
Marcel Noack <marcel.noack.dev@gmail.com>
2021-11-20 16:58:59 +0100

    (fix): redirect all incoming http traffic to https v1

--- server/src/loaders/index.ts
+++ server/src/loaders/index.ts
@@ -5,6 +5,18 @@ import morganLoader from './morgan';
 import Logger from './logger';
 
 export default async ({ expressApp }: { expressApp: express.Express }) => {
+  expressApp.use((req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+      if (req.header('x-forwarded-proto') !== 'https') {
+        res.redirect(`https://${req.header('host')}${req.url}`);
+      } else {
+        next();
+      }
+    } else {
+      next();
+    }
+  });
+
   // Connect to Postgres with postgres-loader
   Logger.info('DB loaded and connected!');
 
