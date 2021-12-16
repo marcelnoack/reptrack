751082e4b67add4db78fec506229a5690f319442
Marcel Noack <marcel.noack.dev@gmail.com>
2021-12-16 22:05:47 +0100

    (feat): create basic angular frontend architecture

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'workouts',
+    loadChildren: () =>
+      import('./modules/workouts/workouts.module').then(
+        (m) => m.WorkoutsModule
+      ),
+  },
+  {
+    path: 'calendar',
+    loadChildren: () =>
+      import('./modules/calendar/calendar.module').then(
+        (m) => m.CalendarModule
+      ),
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { LayoutComponent } from './shared/components/layout/layout.component';
+import { AppBarComponent } from './shared/components/layout/app-bar/app-bar.component';
+import { BottomNavComponent } from './shared/components/layout/bottom-nav/bottom-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    LayoutComponent,
+    AppBarComponent,
+    BottomNavComponent
   ],
   imports: [
     BrowserModule,
