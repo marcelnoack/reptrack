c1b906db02bfbe10590955bd63a4d91f50f96257
Marcel Noack <marcel.noack.dev@gmail.com>
2022-02-22 15:19:56 +0100

    (build): update local angular cli to 13.2.4

---
+++ angular-client/src/app/domains/workouts/pages/workout-details/workout-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rpt-workout-details',
+  templateUrl: './workout-details.component.html',
+  styleUrls: ['./workout-details.component.scss']
+})
+export class WorkoutDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- angular-client/src/app/domains/workouts/workouts.module.ts
+++ angular-client/src/app/domains/workouts/workouts.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { WorkoutsRoutingModule } from './workouts-routing.module';
 import { WorkoutsComponent } from './pages/workouts/workouts.component';
+import { WorkoutDetailsComponent } from './pages/workout-details/workout-details.component';
 
 
 @NgModule({
   declarations: [
-    WorkoutsComponent
+    WorkoutsComponent,
+    WorkoutDetailsComponent
   ],
   imports: [
     CommonModule,
