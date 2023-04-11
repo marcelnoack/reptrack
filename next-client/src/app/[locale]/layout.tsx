266adfa9181a8e4fd2321cfef9b951ece1388e68
Marcel Noack <marcel.noack.dev@gmail.com>
2023-04-11 11:12:29 +0200

    (feat, fix): add CSP and auth pages

--- next-client/src/app/[locale]/layout.tsx
+++ next-client/src/app/[locale]/layout.tsx
@@ -1,51 +1,63 @@
-import { ReactNode } from 'react';
+'use client';
+
+import { ReactNode, useEffect, useState } from 'react';
 import { NextIntlClientProvider } from 'next-intl/client';
-import { notFound } from 'next/navigation';
-import QueryProvider from '@/components/providers/QueryProvider';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import '../globals.scss';
 
+const queryClient = new QueryClient();
 
 interface LocaleProps {
     children: ReactNode;
     params: { locale: string };
 }
 
-export default async function RootLayout( {
-                                               children,
-                                               params: { locale },
-                                           }: LocaleProps ) {
+export default function LocaleLayout( {
+                                         children,
+                                         params: { locale },
+                                     }: LocaleProps ) {
 
-    let messages;
-    try {
-        messages = ( await import( `../../locales/${locale}.json` ) ).default;
-    } catch ( e ) {
-        notFound()
-    }
+    const [ messages, setMessages ] = useState( undefined );
+
+    useEffect( () => {
+        const fetchMessages = async () => {
+            const localeMessages = ( await import( `../../locales/${locale}.json` ) ).default;
+            setMessages( localeMessages );
+        }
+        fetchMessages();
+    }, [ locale ] )
 
     return (
         <html lang={locale}>
         <head>
             {/*TODO: Use current route name*/}
             <title>reptrack-app - current route</title>
-            {/*<meta http-equiv="Content-Security-Policy" content="default-src 'self';"/>*/}
+            <meta http-equiv="Content-Security-Policy" content="default-src 'self';"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <meta name="keywords"
                   content="gym, exercise, workout, training, fitness, assistant, track, repetitions, sets, share, easily"/>
             <meta name="description"
                   content="Gym assistant to track your exercise repetitions, sets and be able to share easily them with others."/>
-            <link rel="shortcut icon" href="/favicon.ico"/>
-            <link
-                href="https://fonts.googleapis.com/icon?family=Material+Icons"
-                rel="stylesheet"
-            />
         </head>
         <body>
-        <QueryProvider>
+        <QueryClientProvider client={queryClient}>
             <NextIntlClientProvider locale={locale} messages={messages}>
-                {children}
+                <div className="h-screen flex flex-col">
+                    <header className="sticky top-0 z-50">
+                        <div className="py-4 mx-4 lg:mx-0">
+                            <div className="flex items-center">
+                                <img src="/Logo3.png" alt="reptrack logo" className="h-12"/>
+                                <h1 className="mx-auto">Current Route</h1>
+                                <div>Actions</div>
+                            </div>
+                        </div>
+                    </header>
+                    <main className="flex-1 overflow-y-auto">{children}</main>
+                    <footer className="sticky bottom-0 z-50">FOOTER</footer>
+                </div>
             </NextIntlClientProvider>
-        </QueryProvider>
+        </QueryClientProvider>
         </body>
         </html>
     );
--- next-client/src/app/[locale]/page.tsx
+++ next-client/src/app/[locale]/page.tsx
@@ -1,5 +1,130 @@
-export default function Home( { params }: {params: {lang: string}} ) {
+'use client';
+
+import { useApi } from '@/lib/data-access/useApi';
+
+export default function Home() {
+    const { usePost } = useApi();
+
+    const { mutate } = usePost<any>( '/auth/signup', JSON.stringify( {
+        user: {
+            username: 'FromNextClient',
+            firstName: 'Marcel',
+            lastName: 'Noack',
+            email: 'somemail@mail.com',
+            password: 'Nockeball98!'
+        }
+    } ) );
+
+    const handleSignup = () => {
+        mutate();
+    };
+
     return <div>
-        {JSON.stringify( params )}
+        <button onClick={() => handleSignup()}>SignUp</button>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
     </div>
 }
