39016fd35afa3061ed77c58b1286cc35e606065b
Marcel Noack <marcel.noack.dev@gmail.com>
2023-04-11 15:50:40 +0200

    Squash commit of the following: feat/next-material-btn
    commit b77c67520d1e5b384950b8cfbec1066793b89c3e
    Author: Marcel Noack <marcel.noack.dev@gmail.com>
    Date:   Tue Apr 11 15:50:05 2023 +0200
    
        (feat)<next-client>: add material btn and input

---
+++ next-client/src/components/material-btn/RptMaterialBtn.tsx
@@ -0,0 +1,17 @@
+'use client';
+export interface RptMaterialBtnProps {
+    icon: string;
+    action: () => void;
+    label?: string;
+    mini?: boolean;
+}
+
+export const RptMaterialBtn = ( props: RptMaterialBtnProps ) => <button
+    data-testid={'rpt-material-btn'}
+    className={'flex items-center justify-center border border-transparent rounded-full bg-zinc-800 text-inherit bg-center outline-0 cursor-pointer transition duration-250 ' +
+        'focus:border-green-500 focus:bg-zinc-400 hover:bg-zinc-400 ' +
+        `${props.mini ? 'p-2': 'p-2.5'} ${props.label?.length && 'rounded-lg pl-3 pr-5'}`}
+    onClick={props.action}>
+    <span className={'material-icons text-white light:text-black'}>{props.icon}</span>
+    {props.label?.length &&  <label>{props.label}</label>}
+</button>
