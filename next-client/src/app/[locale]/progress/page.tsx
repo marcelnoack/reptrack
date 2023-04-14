085b2d2667851fc8fecf15f79cb14c3ff889c988
Marcel Noack <marcel.noack.dev@gmail.com>
2023-04-14 14:24:16 +0200

    Squash commit of the following: feat/next-auth-screens
    commit 72a9245fe8ea8c8594548d836bc37b611e3638d5
    Author: Marcel Noack <marcel.noack.dev@gmail.com>
    Date:   Fri Apr 14 13:31:48 2023 +0200
    
        (refactor)<next-client>: split root layout in auth and content groups
    
    commit 571c1fa41a9421423169640d8fbed8203c80fa60
    Author: Marcel Noack <marcel.noack.dev@gmail.com>
    Date:   Thu Apr 13 21:14:32 2023 +0200
    
        (feat)<next-client>: prepare auth pages
    
    commit 00ecb05c24a093d56f8f2c9780b6f7031e6c4718
    Author: Marcel Noack <marcel.noack.dev@gmail.com>
    Date:   Thu Apr 13 20:28:25 2023 +0200
    
        (feat)<next-client>: add basic login screen

--- next-client/src/app/[locale]/progress/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { useTranslations } from 'next-intl';
-
-export default function Progress() {
-    const tCommon = useTranslations( 'common.prompts' );
-
-    return <>Progress {tCommon( 'enterEmail' )}</>
-}
