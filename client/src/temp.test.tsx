24475e42758a31e93319e7e91ca35ea87ad301e1
Marcel Noack <marcel.noack.dev@gmail.com>
2023-10-27 19:21:33 +0200

    test (client): configure example test

---
+++ client/src/temp.test.tsx
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe( 'some temporary test', () => {
+    it( 'should do a filler test', () => {
+        expect( 1 + 1 ).toEqual( 2 );
+    } )
+} )
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig( {
     test: {
         environment: 'jsdom',
         setupFiles: [ './__tests__/setup.ts' ],
-        include: [ '(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$' ],
         globals: true
     }
 } )
