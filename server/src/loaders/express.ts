50a9effe1c7338cdc46060754a5cd5c40014b0b1
Marcel Noack <marcel.noack.dev@gmail.com>
2024-01-27 11:58:07 +0100

    tsadsa

--- server/src/loaders/express.ts
+++ server/src/loaders/express.ts
@@ -39,7 +39,7 @@ export default ({ app }: { app: express.Application }) => {
       saveUninitialized: false,
       unset: 'keep',
       cookie: {
-        // domain: config.clientUrl,
+        domain: config.clientUrl,
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 360000,
